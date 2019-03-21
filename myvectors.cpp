ae4a5104611befd45387c539dc869e0caa9781ea
Aspendos09 <43933417+Aspendos09@users.noreply.github.com>
2019-03-21 22:37:29 +0300

    En Guncel Versiyon

--- myvectors.cpp
+++ myvectors.cpp
@@ -1,23 +1,27 @@
 #include"myvectors.h"
 #include<stdlib.h>
 #include<iostream>
- MyVectors::MyVectors(int boyut){
+#include<string>
+template<class T>
+ MyVectors<T>::MyVectors(int boyut){
    size=boyut;
- 	vec= new double[size];
-//constructor yazilan boyutta double hafiza ayirdi
+ 	vec= new T[size];
+//constructor yazilan boyutta  hafiza ayirdi
  }
- MyVectors::MyVectors(){
-   size=1;
- 	vec= new double[size];
-//Default constructor 1 tane double hafiza ayirdi
+ template<class T>
+ MyVectors<T>::MyVectors(){
+   size=0;
+ 	vec= new T[size];
+//Default constructor 0 boyutunda hafiza ayirdi
 }
-
-void MyVectors::my_boyut_ayar(int yeni_boyut){
+template<class T>
+void MyVectors<T>::my_boyut_ayar(int yeni_boyut){
   size=yeni_boyut;
   
-  vec=	(double*)realloc(vec,size*sizeof(double)); //doublenin boyutu
+  vec=	(T*)realloc(vec,size*sizeof(T)); //Girilen degerin boyutu
 }//my_boyut_ayar sonu
-void MyVectors::my_assign(int n,double x){
+template<class T>
+void MyVectors<T>::my_assign(int n,T x){//ilk n elemana yollanan x degerini atar
   if(n<size){
   my_boyut_ayar(n);
 }
@@ -30,32 +34,24 @@ else if(n>size)
     vec[i]=x;
   }
 }//my_assign sonu
-
-void MyVectors::my_push_back(double x){
+template<class T>
+void MyVectors<T>::my_push_back(T x){
 
    my_boyut_ayar(size+1);
    vec[size-1]=x;//boyut 11 ise son eleman 10 dur ondan size-1
  }//son eleman eklendi ve boyut buyutuldu
-
-void MyVectors::my_pop_back(){
+template<class T>
+void MyVectors<T>::my_pop_back(){
    vec[size]=0;
 
    my_boyut_ayar(size-1);
  }//son eleman silindi boyut kucultuldu
-
- /*MyVectors::my_insert(int *ary,int knm){
-   if(knm-1>size){
-     my_boyut_ayar(knm-1);  FONKSIYON ISLEVI OGRENİLECEK!!!
-   }
-   for (int i = 0; i < knm; i++) {
-     vec[i]=ary[i];
-   }
- }//yazilan konumdan onceki yerlere yeni elemanlar ekledi gereken boyutu ayarladi*/
-
-void MyVectors::my_erase(int knmb,int knms){
+ 
+template<class T>
+void MyVectors<T>::my_erase(int knmb,int knms){//belirtilen araliktaki degerleri siler boyutu kucultur
 int tempb=knmb;
 int temps=knms;
-for (int i = knmb; i <= knms; i++) {//vector boyutu sorunlari giderilecek
+for (int i = knmb; i <= knms; i++) {
   vec[i]=0;
   
 }
@@ -69,34 +65,23 @@ while (y<=size) {
 my_boyut_ayar(size-(temps-tempb+1));
 }
 
-/*void MyVectors::my_swap(MyVectors vect1,MyVectors vect2){
- // MyVectors tempvec(vect1);
-  //vect1=vect2;
-  //vect2=tempvec;//Copy constructor denemesi umarim calisir CALISMADI 
-  
-  double* tempptr;
-  tempptr =vect1.vec;
-  vect1.vec=vect2.vec;
-  vect2.vec=tempptr;  
-}*/
-void MyVectors::my_clear(){
+template<class T>
+void MyVectors<T>::my_clear(){
 for (int i = 0; i < size; i++) {
   vec[i]=0;
 }
   my_boyut_ayar(0);//elemanlar temizlendi boyut 0 landi
 }
-void MyVectors::my_emplace(int n,double x){
+template<class T>
+void MyVectors<T>::my_emplace(int n,T x){
   my_boyut_ayar(size+1);
   for (int i = size-2; i > n; i--) {
     vec[i+1]=vec[i];//son elemandan itibaren bir ileri tasinir
   }
   vec[n]=x;
 }
-void MyVectors::my_emplace_back(double x){
-  my_push_back(x); //TAM ISLEV SORULACAK
-}
-
-void MyVectors::my_ata(int n,double x){
+template<class T>
+void MyVectors<T>::my_ata(int n,T x){//istenilen konuma yollanan degeri atayan fonksiyon(Boyutu degistirmez!!!)
 	
   if(n>size-1){
     return;
@@ -104,11 +89,29 @@ void MyVectors::my_ata(int n,double x){
   vec[n]=x;
 }
 
-void MyVectors::my_print(int i){
+template<class T>
+void MyVectors<T>::my_print(int i){//belirtilen siradaki elemani ekrana yazan fonksiyon
 	std::cout<<vec[i];
 }
 
-int MyVectors::my_getSize(void){
+template<class T>
+int MyVectors<T>::my_getSize(void){//Cagiran objenin boyutunu donduren fonksiyon
 return size;
 }
+template<class T>
+T MyVectors<T>::my_getData(int n){//n. siradaki elemani donduren fonksiyon
+	return vec[n];
+}
+
+
+
+
+
+
+
+
+template class MyVectors<int>;//C++ nin birden fazla veri tipi icin bu templateleri compilelemesi gerekmektedir
+template class MyVectors<char>;//bu yuzden header dosyasinin sourcesine kullanilacak veri tipleri bu sekilde yazilarak
+template class MyVectors<double>;//compilelemeye zorlanmalidir
+template class MyVectors<float>;
 
--- myvectors.h
+++ myvectors.h
@@ -1,24 +1,48 @@
+
+
+
+template<class T>
 class MyVectors{
 
+	
 	private:
 		int size;
-		double* vec;
+		T* vec;
 
 
 	public:
 		MyVectors();//default constructor
 		MyVectors(int);//constractor vektor boyutunu parametre olarak alacak* +
-		void my_assign(int,double);// Baslangictan itibaren n e kadar olan degerlere istenen sayiyi yazar* +(test edildi demek)
-		void	my_push_back(double);// Vektor sonuna eleman atar ve boyutu arttirir*+
+		
+		void my_assign(int,T);// Baslangictan itibaren n e kadar olan degerlere istenen sayiyi yazar* +(test edildi demek)
+		void	my_push_back(T);// Vektor sonuna eleman atar ve boyutu arttirir*+
 		void	my_pop_back();// son elemani siler boyutu bir azaltir*+
-			//my_insert(int*,int);// FONKSIYON ISLEVI OGRENİLECEK
 		void	my_erase(int,int);// belirtilen araliktaki degerleri siler*
-		//void	my_swap(MyVectors,MyVectors);// Iki vektoru birbirleriyle degistirir.* CALISMIYOR
 		void	my_clear();// Vektorun tum elementlerini siler*+
-		void	my_emplace(int,double);// belirtilen yere eleman ekleyerek vektoru buyutur*+
-		void	my_emplace_back(double);// vektor sonuna eleman ekler ve vektoru buyutur* TAM ISLEV SORULACAK
+		void	my_emplace(int,T);// belirtilen yere eleman ekleyerek vektoru buyutur*+
 		void	my_boyut_ayar(int);//vektor boyutunu ayarlayacak*+
-		void	my_ata(int,double);//belirtilen yerdeki degerin ustune eleman yazar
+		void	my_ata(int,T);//belirtilen yerdeki degerin ustune eleman yazar
 		void	my_print(int);//belirtilen yerdeki degeri ekrana yazar+
-		int	my_getSize(void);//boyutu getirir+
+		int		my_getSize(void);//boyutu getirir+
+		T		my_getData(int n);//n. siradaki elemani dondurur.+
+	
+		MyVectors operator+(MyVectors v1)
+		{
+			MyVectors funvect(0);
+			int count1=0;
+			int count2=0;
+			for(int i=0;i<my_getSize();i++)//Toplama isleminde dondurulecek objeyi oncelikle fonksiyonu cagiran objeye esitler.
+			{
+				funvect.my_push_back(vec[count1]);
+				count1++;
+			}
+			for(int y=0;y<v1.my_getSize();y++)//ilk objeye esitlenen objenin(dondurulecek) ustune isleme giren diger objeyi ekler. 
+			{
+				funvect.my_push_back(v1.my_getData(count2));
+				count2++;
+			}
+			return funvect;//iki objenin toplami olan objeyi dondurur.
+			
+		
+		} //Operator overload fonksiyon sonu
 };
